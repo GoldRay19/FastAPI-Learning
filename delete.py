a016ee83e04ec5a8c59a6af85574a6263359998b
Esha jha <jha.esha2023@gmail.com>
2026-09-04 23:24:38 +0530

    learnt how to update and delete data in db through fastapi

---
+++ delete.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy.orm import sessionmaker,declarative_base,Session
+app=FastAPI()
+database_url="sqlite:///./data.db"
+engine=create_engine(
+    database_url,
+    connect_args={"check_same_thread":False}
+)
+sessionlocal=sessionmaker(bind=engine)
+Base=declarative_base()
+class Todo(Base):
+    __tablename__="todos"
+    id=Column(Integer,primary_key=True,index=True)
+    title=Column(String)
+    completed=Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db=sessionlocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/todos")
+def create_todo(title:str,db:Session=Depends(get_db)):
+    todo=Todo(title=title,completed="False")
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return {
+        "message":"Todo Created",
+        "data":todo
+    } 
+
+#read all data
+@app.get("/todos")
+def get_todos(db:Session=Depends(get_db)):
+    todo=db.query(Todo).all()
+    return {
+        "Total":len(todo),
+        "Data":todo
+
+    }
+
+#read data based on id
+@app.get("/todos/{id}")
+def get_todo(id=int,db:Session=Depends(get_db)):
+    todo=db.query(Todo).filter(Todo.id==id).first()
+    if not todo:
+        raise HTTPException(status_code=404,detail="Todo not found")
+    return todo
+
+#update data 
+@app.put("/todos/{id}")
+def update_todo(id=int,title=str,db:Session=Depends(get_db)):
+    todo=db.query(Todo).filter(Todo.id==id).first()
+    if not todo:
+            raise HTTPException(status_code=404,detail="Todo not found")
+    todo.title=title
+    db.commit()
+    db.refresh(todo)
+    return {
+        "message":"todo updated",
+        "data":todo
+    }
+
+#delete all data
+@app.delete("/todos/{id}")
+def delete_todo(id=int,db:Session=Depends(get_db)):
+    todo=db.query(Todo).filter(Todo.id==id).first()
+    if not todo:
+            raise HTTPException(status_code=404,detail="Todo not found")
+    db.delete(todo)
+    db.commit()
+    return {
+         "message":"data deleted"
+    }
