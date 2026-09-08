8ef9713bfc794538eb394d6410827846bbf6155b
ESHA JHA <jha.esha2024@gmail.com>
2026-09-08 23:29:36 +0530

    Edit cache-api.py

--- cache-api.py
+++ cache-api.py
@@ -12,23 +12,16 @@ last_fetch = 0
 @app.get("/news")
 def get_news():
     global cache_data, last_fetch
-
     start = time.time()
 
     if time.time() - last_fetch > 60:
         print("Fetching Fresh Data")
-
         url= "https://news.ycombinator.com/"
-
         response = requests.get(url)
-
         soup = BeautifulSoup(response.text,"html.parser")
-
         cache_data = [
             item.text for item in soup.find_all("span", class_="titleline")
         ]
-
-
         last_fetch = time.time()
 
     else:
